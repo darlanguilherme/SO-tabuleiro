ed26270cc3969c4b50502a8b8070af81c4d4e975
Darlan Guilherme <darlanguilherme07@gmail.com>
2018-11-21 19:46:14 -0200

    Iniciando game

---
+++ Tabuleiro 10x10/src/tabuleiro/pkg10x10/Tabuleiro10x10.java
@@ -0,0 +1,18 @@
+
+package tabuleiro.pkg10x10;
+
+/**
+ *
+ * @author Darlan Guilherme
+ */
+public class Tabuleiro10x10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaJogo t = new TelaJogo();
+        t.setVisible(true);
+    }
+    
+}
